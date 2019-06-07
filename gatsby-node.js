b43c9a3d046ab45ae49fa2eb1ec1bb08ba181a09
itacir gabral <mu@localhost.localdomain>
2019-06-07 17:02:21 -0400

    v2

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,9 @@
  */
 
 // You can delete this file if you're not using it
+exports.createPages = ({ actions: { createPage } }) => {
+  createPage({
+    path: "/no-data/",
+    component: require.resolve("./src/templates/no-data.js"),
+  })
+}
