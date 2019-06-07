4d88e9237f197ad9987be9ed1b5dbf5b8a70cf93
itacir gabral <mu@localhost.localdomain>
2019-06-07 17:05:20 -0400

    v3

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,4 +10,12 @@ exports.createPages = ({ actions: { createPage } }) => {
     path: "/no-data/",
     component: require.resolve("./src/templates/no-data.js"),
   })
+  createPage({
+    path: "/page-with-context/",
+    component: require.resolve("./src/templates/with-context.js"),
+    context: {
+      title: "We Don’t Need No Stinkin’ GraphQL!",
+      content: "<p>This is page content.</p><p>No GraphQL required!</p>",
+    },
+  })
 }
